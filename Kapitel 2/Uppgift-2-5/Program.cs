fc46192bd3ee8693f6bfb916d94d03e6e358cb4b
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2020-09-16 15:00:46 +0200

    Övat på variabel och inmatning

---
+++ Kapitel 2/Uppgift-2-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läs in ett årtal
+            Console.WriteLine("Ange ett årtal");
+            int årtal = int.Parse(Console.ReadLine());
+
+            // Räkna ut antal år till 2100
+            int antalÅr = 2100 - årtal;
+
+            //skriv ut resultatet
+            Console.WriteLine("Antal år kvar till 2100 är " + antalÅr); 
+        }
+    }
+}
