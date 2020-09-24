8425ebf380119f07f457b863f76289f87af1391a
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2020-09-24 10:31:33 +0200

    Övat på if-sats och boolska uttryck

---
+++ Kapitel 3/Uppgift-3-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läsa i längd
+            Console.Write("Hur lång är du? ");
+            double längd = double.Parse(Console.ReadLine());
+
+            // Svara med om man får eller inte åka
+            if (1.5 < längd && längd <1.9)
+            {
+                Console.WriteLine("du får åka berg-o-dal-bana");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Du får inte åka");
+            }
+
+        }
+    }
+}
---
+++ Kapitel 3/Uppgift-3-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läs in en ålder
+            Console.Write("Hur gammal är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            // Svara om man får extrapriset eller inte
+            // äldre än 65 och mellan 10-18 år
+            if (ålder > 65 || (ålder > 10 && ålder < 18))
+            {
+                System.Console.WriteLine("Du får rabbaten");
+            }
+
+            else 
+            {
+                System.Console.WriteLine("Du får inte rabbaten");
+            }
+
+        }
+    }
+}
