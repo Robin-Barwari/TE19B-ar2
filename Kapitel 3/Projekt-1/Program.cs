eaab18142ee425a2a686b155ab968f8aa557a369
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2020-09-24 11:07:31 +0200

    Projekt

---
+++ Kapitel 3/Projekt-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projekt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Du är den enda som överlevde krashlandningen och du befinner dig på ett öde ö, du har ingen mat eller vatten. Ska du 1. gå ut och leta efter mat och vatten på den här Ön eller 2. stanna vid flyget?");
+            string val = Console.ReadLine();
+            val = val.ToLower();
+            if (val == "1")
+            {
+                Console.WriteLine("");
+            }
+            else 
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
