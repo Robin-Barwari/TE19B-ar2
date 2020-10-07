1a8ed22df7bb3605f1032c1f135cc66dc92d1fc1
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2020-10-07 14:56:23 +0200

    Loop

---
+++ Kapitel 4/Exempel-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Slumpa fram ett tal 1-6
+            Random tärning = new Random ();
+            int slumptal = tärning.Next(1,7);
+
+            // Upprapa tills rätt svar gissats
+            while (true)
+            {  
+            // Läs in användarens gissning
+            Console.WriteLine("Gissa ett tal (1-6) ?");
+            int gissning = int.Parse(Console.ReadLine());
+
+            // Om användaren gissat rätt?
+            if (gissning == slumptal)
+            {
+            Console.WriteLine("Hurra! Du har gissat rätt");
+            }
+            else 
+            {
+            Console.WriteLine("Fel gissat! Försök igen!");
+            }
+            }
+        }
+    }
+}
---
+++ Kapitel 4/Exempel-2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Deklarera variabler
+            string val = "";
+
+        // Presentera en ny meny som visas tills användaren säger stopp
+        while (val != "3")
+        {
+            Console.WriteLine("Välj något av följande val:");
+            Console.WriteLine("1. addera två tal ");
+            Console.WriteLine("2. multiplicera två tal ");
+            Console.WriteLine("3. avsluta ");
+
+            // Läs in användarens val
+            val = Console.ReadLine();
+
+            // Om val = 1
+            if (val == "1")
+            {
+                Console.WriteLine("Skriv in två tal på var sin rad");
+                double tal1 = double.Parse(Console.ReadLine());
+                double tal2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{tal1} + {tal2} = {tal1 + tal2}");
+            }
+
+            // Om val = 2
+            else if (val == "2")
+            {
+                Console.WriteLine("Skriv in två tal på var sin rad");
+                double faktor1 = double.Parse(Console.ReadLine());
+                double faktor2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{faktor1} * {faktor2} = {faktor1 * faktor2}");
+            }
+
+            // Om val = 3
+            else if (val == "3")
+            {
+                Console.WriteLine("Stick härifrån.");
+                break;
+            }
+
+            // Annars
+                else
+            {
+                 Console.WriteLine("Du valde inte ett giltigt alternativ.");
+            }
+
+            switch (val)
+            {
+                case "1":
+                Console.WriteLine("Skriv in två tal på var sin rad");
+                double tal1 = double.Parse(Console.ReadLine());
+                double tal2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{tal1} + {tal2} = {tal1 + tal2}");
+                break;
+
+                case "2":
+                Console.WriteLine("Skriv in två tal på var sin rad");
+                double faktor1 = double.Parse(Console.ReadLine());
+                double faktor2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{faktor1} * {faktor2} = {faktor1 * faktor2}");
+                break;     
+
+                case "3":
+                Console.WriteLine("Stick härifrån.");
+                break;
+
+                default:
+                Console.WriteLine("Var vänlig och välj mellan 1, 2, 3");
+                break;
+
+            }
+        }
+        }
+    }
+}
