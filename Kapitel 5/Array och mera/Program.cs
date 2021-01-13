efa211deaaa13e183b33ab406ef7769bda510cba
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-01-13 15:00:52 +0100

    Saker

---
+++ Kapitel 5/Array och mera/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array_och_mera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumber = {4, 8, 15, 16, 23, 42};
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Rahim";
+            friends[2] = "Huska";
+            friends[3] = "Bert";
+            friends[4] = "Ernie";
+
+            Console.WriteLine( luckyNumber[1]);
+            Console.ReadLine();
+        }
+    }
+}
