d13de68042482744e2adffe44e58a77c32954ec0
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-01-14 11:12:23 +0100

    Träna på filhantering

---
+++ Labbar/Filhantering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Filhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriva till en textfil
+            File.WriteAllText("fil1.text" , "Nu snöar det igen! Kul!");
+
+            // Läsa innehållet av en text
+            string allText = File.ReadAllText("./Filhantering.csproj");
+
+            // Skriv ut innehållet
+            Console.WriteLine($"Innehållet: \n{allText}");
+
+            // Fråga användaren om en filnamn
+            Console.Write("Ange filnamn: ");
+            string filnamn = Console.ReadLine();
+
+            // Kontrollera att filen finns!
+            if (File.Exists(filnamn))
+            {
+                string texten = File.ReadAllText(filnamn);
+                Console.WriteLine(texten);
+            }
+            else
+            {
+                Console.WriteLine($"Hittade inte filen \"{filnamn}\"");
+            }
+        }
+    }
+}
