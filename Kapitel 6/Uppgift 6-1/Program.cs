65ef4aabb657e510ab6e3badd632a8969f178d82
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-02-25 11:06:13 +0100

    Metoder

---
+++ Kapitel 6/Uppgift 6-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SkrivTalIKvadrat(5);
+
+            SkrivTalIKvadrat(40);
+
+            SkrivTalIKvadrat(25);
+
+        }
+        static void SkrivTalIKvadrat(int tal) {
+            Console.WriteLine($"Talet {tal} upphöjt med två är {tal * tal}");
+        }
+    }
+}
---
+++ Kapitel 6/Uppgift 6-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Undrat hur din text skulle se ut baklänges?");
+            Console.WriteLine("Ange din text så får vi se.");
+            SkrivBaklänges(Console.ReadLine());
+        }
+        static void SkrivBaklänges (string meddelande)
+        {
+            for (int i = meddelande.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
