938e56a5ad6c511888fbbf00b7a942c886cc24cf
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-02-25 11:05:59 +0100

    Metoder

---
+++ Kapitel 6/Uppgift 6-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vi ska räkna vad alla tal mellan två nummer är i kvadrat");
+            // inte färdig
+        }
+        static void SkrivTalIKvadratMellan(int nedreGräns, int övreGräns)
+        {
+            for (int i = nedreGräns; i <= övreGräns; i++)
+            {
+                Console.WriteLine($"talet {i} uphöjt med två är {i * i}");
+            }
+        }
+    }
+}
