543c25c1ab61a61d63920cc3fa5275e10e53b9a7
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-03-17 14:59:30 +0100

    kommentarer på metoder

--- Kapitel 6/Exempel-3/Program.cs
+++ Kapitel 6/Exempel-3/Program.cs
@@ -11,13 +11,23 @@ namespace Exempel_3
             string omvändText = VändText("Programmering");
         }
 
-        // Metod för att summera två tal
+        /// <summary>
+        /// Etoden summera två tal
+        /// </summary>
+        /// <param name="x">Försat heltal</param>
+        /// <param name="y">Andra heltal</param>
+        /// <returns>Summan</returns>
         static int Summera(int x, int y)
         {
             int z = x + y;
             return z;
         }
 
+        /// <summary>
+        /// Metod för att använda på en text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         static string VändText(string text)
         {
             //...
--- Kapitel 6/Uppgift 6-3/Program.cs
+++ Kapitel 6/Uppgift 6-3/Program.cs
@@ -9,6 +9,11 @@ namespace Uppgift_6_3
             Console.WriteLine("Vi ska räkna vad alla tal mellan två nummer är i kvadrat");
             // inte färdig
         }
+        /// <summary>
+        /// Metoden skriver ut talet i kvadrat
+        /// </summary>
+        /// <param name="nedreGräns"></param>
+        /// <param name="övreGräns"></param>
         static void SkrivTalIKvadratMellan(int nedreGräns, int övreGräns)
         {
             for (int i = nedreGräns; i <= övreGräns; i++)
