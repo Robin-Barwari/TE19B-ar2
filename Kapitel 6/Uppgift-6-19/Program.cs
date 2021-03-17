ebc0b76a389b3adb13159d993d3983c5d644f3c3
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-03-17 13:43:01 +0100

    metoder

---
+++ Kapitel 6/Uppgift-6-19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_6_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Störst(34, 43));
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            return tal2;
+        }
+    }
+}
---
+++ Kapitel 6/Uppgift-6-20/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Uppgift_6_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menyVal = "";
+            while (menyVal != "3")
+            {
+            Console.WriteLine("Välj ett utav de tre alternativen");
+            Console.WriteLine("1. Addera tre tal");
+            Console.WriteLine("2. Största talet av två tal");
+            Console.WriteLine("3. Avsluta");
+            menyVal = Console.ReadLine();
+
+            switch (menyVal)
+            {
+                case "1":
+                MenyvalAddera();
+                break;
+
+                case "2":
+                MenyvalStörstaTalet();
+                break;
+
+                case "3":
+                break;
+
+                default:
+                Console.WriteLine("Felaktig svar, försök igen");
+                break;
+            }
+            }
+        }
+
+         static void MenyvalAddera()
+        {
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            int tal3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Addera(tal1, tal2, tal3));
+        }
+
+        static void MenyvalStörstaTalet()
+        {
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Störst(tal1, tal2));
+
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            return tal2;
+        }
+    }
+}
